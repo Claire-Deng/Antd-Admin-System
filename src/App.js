be13853d9e00164f4bf50189a7a9b6a64fb81470
Claire-Deng <492074030@qq.com>
2020-02-25 22:50:25 +0800

    router ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { BrowserRouter, Switch, Link, Route } from 'react-router-dom';
+// import {Route,Switch} from 'react-router-dom';
+import PrivateRoute from './pages/components/PrivateRoute';
+import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-
-    </div>
+      <BrowserRouter>
+          <Switch>
+              <Route path='/login' component={Login}/>
+              <PrivateRoute path='/home' component={Home}/>
+          </Switch>
+      </BrowserRouter>
   );
 }
 
